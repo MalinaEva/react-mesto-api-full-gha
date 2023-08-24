4ad5ee8a7815f52cf8f4ab2a7de70f78f7268374
MalinaEva <teqi@yandex.ru>
2023-08-25 01:23:21 +0700

    fix: codestyle

--- backend/utils/rateLimitConfig.js
+++ backend/utils/rateLimitConfig.js
@@ -1,7 +1,7 @@
 const config = {
   windowMs: 15 * 60 * 1000,
   max: 100,
-  message: 'Слишком много запросов с одного IP, попробуйте позже'
-}
+  message: 'Слишком много запросов с одного IP, попробуйте позже',
+};
 
 module.exports = config;
